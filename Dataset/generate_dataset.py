d5633f5ff8b4f4235d492281ca4de8699821899d
wnourse05 <wrn13@case.edu>
2023-12-20 13:45:28 -0500

    Added dataset

---
+++ Dataset/generate_dataset.py
@@ -0,0 +1,67 @@
+import pickle
+import os
+import cv2
+import numpy as np
+
+def process_file(file_path, index, num_frames, scaled_width, scaled_height):
+    # Load the trial data
+    data = pickle.load(open(file_path, 'rb'))
+    print(index)
+
+    # If velocity is positive, skip
+    vel = data['cmd_vel'][0]
+    if vel > -0.1:  # 0.05 is too slow, stutters
+        print('Wrong velocity')
+        return None, None, None
+
+    images = data['image']  # set of 90 images
+    # Skip trials where there is no change over the course of the trial
+    no_change = all(np.array_equal(i, images[0]) for i in images)
+    if no_change:
+        print('No change in image data')
+        return None, None, None
+
+    left_frames = []
+    right_frames = []
+
+    for i in range(num_frames):
+        print('%i    Frame %i/%i' % (index, i + 1, num_frames))
+        img = data['image'][i]  # grab frame
+
+        # Resize the image using the fastest interpolation method (cv2.INTER_NEAREST)
+        scaled_image = cv2.resize(img, (2 * scaled_width, scaled_height), interpolation=cv2.INTER_NEAREST)
+        scaled_image_green = scaled_image[:, :, 1]  # isolate green channel
+        # store scaled frame in buffers
+        left_frames.append(scaled_image_green[:, :32])
+        right_frames.append(scaled_image_green[:, 32:])
+
+    return left_frames, right_frames, vel
+
+def process_files_in_directory(directory_path):
+    # List all files in the directory
+    file_list = os.listdir(directory_path)
+    num_files = len(file_list)
+    num_frames = 90  # number of frames
+    scaled_width, scaled_height = 32, 24  # size of one eye
+    data = None
+    labels = None
+
+    # Iterate through each file and perform the operation
+    for i in range(num_files):  # Fix the loop range
+        file_path = os.path.join(directory_path, file_list[i])  # get path
+        left, right, vel = process_file(file_path, i, num_frames, scaled_width, scaled_height)  # get trial
+        if vel is not None:
+            if data is None:
+                data = np.stack([left, right], axis=3)
+                labels = np.array([vel])
+            else:
+                data_left = np.array(left)[:, :, :, np.newaxis]  # Add a new axis for stacking
+                data_right = np.array(right)[:, :, :, np.newaxis]  # Add a new axis for stacking
+                data = np.concatenate([data, data_left, data_right], axis=3)
+                labels = np.hstack([labels, np.array([vel])])
+
+    dataset = {'data': data, 'labels': labels}
+    pickle.dump(dataset, open('dataset.p', 'wb'))
+
+directory_path = '../FlyWheelTrials/'
+process_files_in_directory(directory_path)
---
+++ Dataset/sort_dataset.py
@@ -0,0 +1,85 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+rng = np.random.default_rng(seed=0) # Random number generator
+
+data = pickle.load(open('dataset.p', 'rb')) # Load data
+
+images = np.moveaxis(data['data'],0,2)  # Pull image data, and reorder axes
+
+labels = np.repeat(data['labels'], 2)    # Double the labels, because the dataset has left and right split
+# i_sorted = np.argsort(labels)
+# labels_sorted = labels[i_sorted]
+
+num_samples = len(labels)    # Number of samples
+print('%i Samples'%num_samples)
+counts, _ = np.histogram(labels) # Number of each velocity available
+bins = np.unique(labels) # velocity categories
+num_categories = len(bins)
+
+i_random = rng.choice(num_samples, num_samples) # randomized, non-repeating indices
+
+# Test/Train breakdown
+num_test = 40*9
+num_per_category = 40
+num_train = num_samples - num_test
+print('Test Size:  %i'%num_test)
+print('Train Size: %i'%num_train)
+
+set_test = None
+set_train = None
+labels_test = None
+labels_train = None
+
+for i in range(num_samples):
+    print('%i/%i'%(i+1,num_samples))
+    vel = labels[i_random[i]]
+    image = images[:,:,:,i_random[i]]
+    name = 'gifs/%.2f_%i.gif'%(vel,i)
+    frames = []
+    for j in range(90):
+        # print('j = %i'%j)
+        if j == 0:
+            frames = [Image.fromarray(image[:,:,j])]
+        else:
+            frames.append(Image.fromarray(image[:,:,j]))
+    frames[0].save(name, save_all=True, append_images=frames[1:], duration=50, loop=0)
+    if labels_test is None:
+        print('Creating Test Set')
+        labels_test = np.array([vel])
+        set_test = image
+    elif np.count_nonzero(labels_test == vel) < num_per_category:
+        print('Adding to Test Set')
+        labels_test = np.hstack([labels_test,vel])
+        if len(set_test.shape) > 3:
+            set_test = np.concatenate([set_test,image[...,None]],axis=3)
+        else:
+            set_test = np.stack([set_test,image], axis=3)
+    else:
+        if labels_train is None:
+            print('Creating Training Set')
+            labels_train = np.array([vel])
+            set_train = image
+        else:
+            print('Adding to Training Set')
+            labels_train = np.hstack([labels_train, vel])
+            if len(set_train.shape) > 3:
+                set_train = np.concatenate([set_train, image[..., None]], axis=3)
+            else:
+                set_train = np.stack([set_train, image], axis=3)
+
+print(len(labels_test))
+print(len(labels_train))
+
+data_train = {'data':set_train, 'labels': labels_train}
+data_test = {'data':set_test, 'labels': labels_test}
+
+pickle.dump(data_test, open('set_test.p', 'wb'))
+pickle.dump(data_train, open('set_train.p', 'wb'))
+
+# plt.figure()
+# plt.bar(bins, counts)
+# print('Data Loaded')
+# plt.show()

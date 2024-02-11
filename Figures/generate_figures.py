96e638276ad6390325b7211269ce52b025d15bb5
wnourse05 <wrn13@case.edu>
2024-02-11 16:52:57 -0500

    Image latency

--- Figures/generate_figures.py
+++ Figures/generate_figures.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import pickle
+import numpy as np
 
 def desired_performance():
-    data = pickle.load(open('../Data/desiredPerformance.p', 'rb'))
+    data = pickle.load(open('desiredPerformance.p', 'rb'))
 
     plt.figure()
     plt.plot(data['samplePts'], data['magnitude'])
@@ -10,6 +11,27 @@ def desired_performance():
     plt.ylabel('Neural Average Magnitude')
     plt.title('Desired Neural Magnitude Across Rotational Velocity')
 
+def camera_latency():
+    full_row = 1232
+    full_col = 3280
+    rows = np.array([1232, 616, 308, 154, 77, 38, 24, 19])
+    cols = np.array([3280, 1640, 820, 410, 205, 102, 64, 51])
+    scale = full_row/rows
+    mine = full_row/24
+    latency_near = np.array([4, 4, 5, 5, 4, 4, 6, 4])
+    latency_area = np.array([4, 5, 5, 7, 7, 22, 25, 26])
+
+    plt.figure()
+    plt.title('Image Scaling Performance')
+    plt.plot(scale, latency_near, label='Nearest Neighbor')
+    plt.plot(scale, latency_area, label='Area')
+    plt.axvline(x=mine, color='black', linestyle='--', label='[24x64]')
+    plt.xlabel('Reduction Factor')
+    plt.ylabel('Image Processing Latency (ms)')
+    plt.xscale('log')
+    plt.legend()
+
 if __name__ == "__main__":
     desired_performance()
+    camera_latency()
     plt.show()
--- desired_tuning.py
+++ desired_tuning.py
@@ -5,4 +5,4 @@ sample_pts = np.array([0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5])
 desired_tuning = np.linspace(1,0.1,num=len(sample_pts))
 desired_performance = {'samplePts': sample_pts,
                        'magnitude': desired_tuning}
-pickle.dump(desired_performance, open('Data/desiredPerformance.p', 'wb'))
+pickle.dump(desired_performance, open('Figures/desiredPerformance.p', 'wb'))
